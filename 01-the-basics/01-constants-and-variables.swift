9d116c10f8306ba75c7295bbd43bc428e6a4fa19
Junfeng (Jeff) Li <autozimu@gmail.com>
2015-12-16 21:48:53 -0500

    Emoji.

---
+++ 01-the-basics/01-constants-and-variables.swift
@@ -0,0 +1,13 @@
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttemp = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
