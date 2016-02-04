d384bcd098291e7c5c3a5e028ed18eb85c147f77
Jeff Li <junfengl@ca.ibm.com>
2016-02-04 15:36:43 -0500

    Type Casting.

---
+++ 19-type-casting/04-AnyObject.swift
@@ -0,0 +1,74 @@
+class MediaItem {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Movie: MediaItem {
+    var director: String
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name: name)
+    }
+}
+
+class Song: MediaItem {
+    var artist: String
+    init(name: String, artist: String) {
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let someObjects: [AnyObject] = [
+    Movie(name: "2001: A Space Odyssey", director: "Stanley Kubrick"),
+    Movie(name: "Moon", director: "Duncan Jones"),
+    Movie(name: "Alien", director: "Ridley Scott"),
+]
+
+for object in someObjects {
+    let movie = object as! Movie
+    print("Movie: '\(movie.name)', dir. \(movie.director)")
+}
+
+// for movie in someObjects as! [Movie] {
+//     print("Movie: '\(movie.name)', dir. \(movie.director)")
+// }
+
+// Any
+
+var things = [Any]()
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append(3.14159)
+things.append("hello")
+things.append((3.0, 5.0))
+things.append(Movie(name: "Ghostbusters", director: "Ivan Reitman"))
+things.append({ (name: String) -> String in "Hello, \(name)" })
+
+for thing in things {
+    switch thing {
+    case 0 as Int:
+        print("zero as an Int")
+    case 0 as Double:
+        print("zero as a Double")
+    case let someInt as Int:
+        print("an integer value of \(someInt)")
+    case let someDouble as Double where someDouble > 0:
+        print("a positive double value of \(someDouble)")
+    case is Double:
+        print("some other double value that I don't want to print")
+    case let someThing as String:
+        print("a string value of \"\(someThing)\"")
+    case let (x, y) as (Double, Double):
+        print("an (x, y) point at \(x), \(y)")
+    case let movie as Movie:
+        print("a movie called '\(movie.name)', dir. \(movie.director)")
+    case let stringConverter as String -> String:
+        print(stringConverter("Michael"))
+    default:
+        print("something else")
+    }
+}
